ecd68a63e0d65c92b9df86c7e485aa8bbb78f33a
Diego F Garcia <gadifer54@gamil>
2024-03-13 09:58:29 -0400

    agregandoBotones

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 
 import './App.css';
+import Boton from './components/Boton';
 import freeCodeCampLogo from './imagenes/freecodecamplogo.png'
+
+
+
 function App() {
+  const manejarClick= () => {
+     console.log('Click');
+  }
+
+  const reiniciarContador= () => {
+    console.log('Reiniciar');
+  }
   return (
     <div className="App">
        <div className='freecodecamp-logo-contenedor'>
@@ -9,6 +20,16 @@ function App() {
              src={freeCodeCampLogo} 
              alt='logo de freeCodeCamp'/>
 
+       </div>
+       <div className='contenedor-principal'>
+          <Boton
+          texto='Click'
+          esBtoondeClick={true}
+          manejarClick={manejarClick}/>
+          <Boton 
+          texto='Reiniciar'
+          esBotonDeClick={false}
+          manejarClick={reiniciarContador}/> 
        </div>
     </div>
   );
